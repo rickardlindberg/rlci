4dedb80f1b2f91dbf6cb5c9afe6ee57d7980ebaf
Rickard Lindberg <rickard@rickardlindberg.me>
2022-04-18 15:42:38 +0200

    Flesh out more server structure.

--- rlci/server/src/server.py
+++ rlci/server/src/server.py
@@ -8,62 +8,63 @@ class InMemoryObjectStore:
     def __init__(self):
         self.objects = {}
 
-    def modify_object(self, name, fn):
-        fn(self.objects[name])
-
-    def read_object(self, name):
-        return self.objects[name]
-
-    def create_object(self, contents, name=None):
+    async def create_object(self, contents, name=None):
         if name is None:
             name = uuid.uuid4().hex
         self.objects[name] = contents
         return name
 
+    async def modify_object(self, name, fn):
+        fn(self.objects[name])
+
+    async def read_object(self, name):
+        return self.objects[name]
+
 class PipelineDB:
 
-    def __init__(self, db):
-        self.db = db
-        self.db.create_object({"pipelines": {}}, "index")
+    def __init__(self, store):
+        self.store = store
 
-    def store_pipelines(self, pipelines):
+    async def init(self):
+        await self.store.create_object({"pipelines": {}}, "index")
+
+    async def store_pipelines(self, pipelines):
         return [
-            self.store_pipeline(pipeline[1]["name"], pipeline[2:])
+            await self.store_pipeline(pipeline[1]["name"], pipeline[2:])
             for pipeline in pipelines
         ]
 
-    def get_pipeline(self, pipeline_id):
-        return self.db.read_object(pipeline_id)
+    async def get_pipeline(self, pipeline_id):
+        return await self.store.read_object(pipeline_id)
 
-    def get_execution(self, execution_id):
-        return self.db.read_object(execution_id)
+    async def get_execution(self, execution_id):
+        return await self.store.read_object(execution_id)
 
-    def store_pipeline(self, name, pipeline):
-        pipeline_id = self.db.create_object({"def": pipeline, "executions": []})
-        self.db.modify_object("index", lambda index: index["pipelines"].__setitem__(
-            "name",
-            self.db.create_object({"instances": [pipeline_id]})
-        ))
+    async def store_pipeline(self, name, pipeline):
+        pipeline_id = await self.store.create_object({"def": pipeline, "executions": []})
+        foo = await self.store.create_object({"instances": [pipeline_id]})
+        await self.store.modify_object("index", lambda index:
+            index["pipelines"].__setitem__(
+                "name",
+                foo
+            )
+        )
         return pipeline_id
 
-    def trigger(self, values):
+    async def trigger(self, values):
         executions = []
-        for pipeline_id in self.db.read_object("index")["pipelines"].values():
-            foo = self.db.read_object(pipeline_id)["instances"][0]
-            for ast in self.db.read_object(foo)["def"]:
+        for pipeline_id in (await self.store.read_object("index"))["pipelines"].values():
+            foo = (await self.store.read_object(pipeline_id))["instances"][0]
+            for ast in (await self.store.read_object(foo))["def"]:
                 if ast[0] == "Node":
                     for trigger in ast[2]["triggers"]:
                         if self.trigger_matches(trigger, values):
-                            y = self.create_execution()
+                            y = await self.create_execution()
                             executions.append(y)
-                            self.db.modify_object(foo, lambda pipeline:
+                            await self.store.modify_object(foo, lambda pipeline:
                                 pipeline["executions"].append(y)
                             )
         return executions
-
-    def create_execution(self):
-        return self.db.create_object({
-        })
 
     def trigger_matches(self, trigger, values):
         for key, value in trigger.items():
@@ -71,31 +72,53 @@ class PipelineDB:
                 return False
         return True
 
-if __name__ == "__main__":
-    db = PipelineDB(InMemoryObjectStore())
-    async def handle_request(reader, writer):
+    async def create_execution(self):
+        return await self.store.create_object({
+        })
+
+class Server:
+
+    def __init__(self, db):
+        self.db = db
+
+    def serve_forever(self):
+        asyncio.run(self.server())
+
+    async def server(self):
+        await self.db.init()
+        server = await asyncio.start_server(
+            self.handle_request,
+            host="localhost",
+            port=9000
+        )
+        print("listening on port 9000")
+        sys.stdout.flush()
+        async with server:
+            await server.serve_forever()
+
+    async def handle_request(self, reader, writer):
         request_data = await reader.readline()
         try:
             request = json.loads(request_data)
             if request["message"] == "store_pipelines":
                 response = {
                     "status": "ok",
-                    "ids": db.store_pipelines(request["payload"])
+                    "ids": await self.db.store_pipelines(request["payload"])
                 }
             elif request["message"] == "trigger":
                 response = {
                     "status": "ok",
-                    "executions": db.trigger(request["payload"])
+                    "executions": await self.db.trigger(request["payload"])
                 }
             elif request["message"] == "get_pipeline":
                 response = {
                     "status": "ok",
-                    "pipeline": db.get_pipeline(request["pipeline_id"])
+                    "pipeline": await self.db.get_pipeline(request["pipeline_id"])
                 }
             elif request["message"] == "get_execution":
                 response = {
                     "status": "ok",
-                    "execution": db.get_execution(request["execution_id"])
+                    "execution": await self.db.get_execution(request["execution_id"])
                 }
             else:
                 raise ValueError(f"Unknown message {request['message']}")
@@ -106,10 +129,6 @@ if __name__ == "__main__":
         await writer.drain()
         writer.close()
         await writer.wait_closed()
-    async def server():
-        server = await asyncio.start_server(handle_request, host="localhost", port=9000)
-        print("listening on port 9000")
-        sys.stdout.flush()
-        async with server:
-            await server.serve_forever()
-    asyncio.run(server())
+
+if __name__ == "__main__":
+    Server(PipelineDB(InMemoryObjectStore())).serve_forever()
