d70feb1394af98da05f7371ebd50d7c92d831fad
Rickard Lindberg <rickard@rickardlindberg.me>
2022-06-05 21:13:06 +0200

    Refactor: Clarify tests for Terminal class.

--- rlci.py
+++ rlci.py
@@ -52,13 +52,21 @@ class Terminal(Observable):
     """
     I represent a terminal emulator to which text can be printed.
 
-    I print text to stdout:
+    I write text to stdout:
 
     >>> subprocess.run([
-    ...     "python", "-c"
-    ...     "import zero; zero.Terminal().print_line('hello')"
-    ... ], stdout=subprocess.PIPE).stdout
+    ...     "python", "-c",
+    ...     "import rlci; rlci.Terminal().print_line('hello')"
+    ... ], stdout=subprocess.PIPE, check=True).stdout
     b'hello\\n'
+
+    The null version of me doesn't write anything to stdout:
+
+    >>> subprocess.run([
+    ...     "python", "-c",
+    ...     "import rlci; rlci.Terminal.create_null().print_line('hello')"
+    ... ], stdout=subprocess.PIPE, check=True).stdout
+    b''
 
     I log the lines that I print:
 
