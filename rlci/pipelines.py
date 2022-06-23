0943359e55e0f7e5f3cd4f8df79367b70232d653
Rickard Lindberg <rickard@rickardlindberg.me>
2022-06-23 22:50:43 +0200

    Refactor: Remove unused code Runtime.sh.

--- rlci/pipelines.py
+++ rlci/pipelines.py
@@ -9,33 +9,6 @@ from rlci.infrastructure import Terminal, Process
 class Runtime(Observable):
 
     """
-    ## Shell commands
-
-    I can run shell commands:
-
-    >>> Runtime().sh('echo hello')
-    b'hello\\n'
-
-    I fail if command fails:
-
-    >>> Runtime().sh('exit 1')
-    Traceback (most recent call last):
-        ...
-    subprocess.CalledProcessError: Command 'exit 1' returned non-zero exit status 1.
-
-    The null version of me, runs no shell commands:
-
-    >>> Runtime.create_null().sh('echo hello')
-    b''
-
-    I log commands:
-
-    >>> events = Events()
-    >>> pipeline = events.listen(Runtime.create_null())
-    >>> _ = pipeline.sh("echo hello")
-    >>> events
-    SH => 'echo hello'
-
     ## Workspace commands
 
     I can create empty workspaces:
@@ -58,11 +31,6 @@ class Runtime(Observable):
     def __init__(self, subprocess=subprocess):
         Observable.__init__(self)
         self.subprocess = subprocess
-
-    def sh(self, command):
-        self.notify("SH", command)
-        return self.subprocess.run(command, shell=True,
-        stdout=self.subprocess.PIPE, check=True).stdout
 
     @contextlib.contextmanager
     def workspace(self):
